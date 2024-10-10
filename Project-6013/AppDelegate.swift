231f0ddf79828a27b2a8a6210f73643373c19eb5
William <williamhalliday_pa@outlook.com>
2024-10-10 11:46:15 +0100

    Initial Commit
    Lazygit testing, initial commit includes the first version of README.txt

--- Project-6013/AppDelegate.swift
+++ Project-6013/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RealmSwift;
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        do
+        {
+            let realm = try Realm();
+        }
+        catch
+        {
+            print("error caught attempting to connect to Realm! \(error)")
+        }
         return true
     }
 
---
+++ Project-6013/README.txt
@@ -0,0 +1,4 @@
+An iOS app built in Swift, utilising aspects of ML, NLP, AR, Cloud solutions,
+Relation databases and more.
+
+This is a project intended for the 6013 module at OBU, but will hopefully reach broader horizons in the future.
