50ce64213dbcb0a189c535f194824f375d767f31
William <williamhalliday_pa@outlook.com>
2025-02-18 16:25:19 +0000

    Initial Commit

---
+++ Compute/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Compute
+//
+//  Created by William Halliday on 18/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
